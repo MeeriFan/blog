db1481554c819561ef7a0ffdb6bae218edc303bb
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-11 12:43:03 +0200

    inheritance solved(?)

--- blog_classes.py
+++ blog_classes.py
@@ -7,40 +7,43 @@ class User(object):
 
 	posts = []
 
-	def __init__(self, email='', first_name='', last_name='', posts):
+	def __init__(self, email, first_name, last_name, posts):
 		self.email = str(email)
 		self.first_name = str(first_name)
-		self.last_name = last_name
-		self.posts = posts
+		self.last_name = str(last_name)
+		self.posts = self.posts()
 
 	def full_name(self):
 		return self.first_name + ' ' + self.last_name
 
 	def posts(self):
-		pass
+		for single_post in Post:
+			if self == Post.author:
+				self.posts.append(single_post)
+		return self.posts
 
-class Post(object):
+class Post(User):
 	"""
 
 	"""
 
-	def __init__(self, title='', body='' , publish_date=None):
+	def __init__(self, title, body, publish_date=None):
 		self.title = str(title)
 		self.body = str(body)
 		self.publish_date = datetime(publish_date)
 		self.created_at = datetime.now()
-		self.author = ''
+		self.author = User
 
 		def is_published(self):
 			return publish_date != None
 
-class Comment(object):
+class Comment(Post):
 	"""
 
 	"""
 
-	def __init__(self, author_email='', body='', post):
-		self.author_email = author_email
-		self.body = body
+	def __init__(self, author_email='', body=''):
+		self.author_email = str(author_email)
+		self.body = str(body)
 		self.created_at = datetime.now()
-		self.post = posts
+		self.post = Post
