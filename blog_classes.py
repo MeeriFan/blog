ec7a2aaa38b3fbacba91b56dc5861bffcc89b7be
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-11 13:45:24 +0200

    figured out all methods. should work now except for posts method in user class, always getting an empty list....

--- blog_classes.py
+++ blog_classes.py
@@ -1,13 +1,14 @@
 from datetime import datetime
+from datetime import date
 
 class User(object):
 	"""
 
 	"""
 
-	posts = []
+	all_posts = []
 
-	def __init__(self, email, first_name, last_name, posts):
+	def __init__(self, email, first_name, last_name):
 		self.email = str(email)
 		self.first_name = str(first_name)
 		self.last_name = str(last_name)
@@ -17,33 +18,39 @@ class User(object):
 		return self.first_name + ' ' + self.last_name
 
 	def posts(self):
-		for single_post in Post:
-			if self == Post.author:
-				self.posts.append(single_post)
-		return self.posts
+		for single_post in Post.list_of_posts:
+			if self == single_post.author:
+				self.all_posts.append(single_post)
+		return self.all_posts
 
-class Post(User):
+class Post(object):
 	"""
 
 	"""
+	list_of_posts = []
 
-	def __init__(self, title, body, publish_date=None):
+	def __init__(self, title, body, User, year=0, month=0, day=0):
 		self.title = str(title)
 		self.body = str(body)
-		self.publish_date = datetime(publish_date)
+		try:
+			self.publish_date = date(year, month, day)
+		except ValueError:
+			self.publish_date = ''
 		self.created_at = datetime.now()
 		self.author = User
+		Post.list_of_posts.append(self)
 
-		def is_published(self):
-			return publish_date != None
+	def is_published(self):
+		return self.publish_date != ''
 
-class Comment(Post):
+
+class Comment(object):
 	"""
 
 	"""
 
-	def __init__(self, author_email='', body=''):
-		self.author_email = str(author_email)
+	def __init__(self, body, User, Post):
+		self.author_email = User.email
 		self.body = str(body)
 		self.created_at = datetime.now()
 		self.post = Post
