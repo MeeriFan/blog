ddda9b5eea1e2df24af84d5f676bff48a5748f1f
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-11 16:31:59 +0200

    edit posts function, reviewed code with tobias, added comments

--- blog_classes.py
+++ blog_classes.py
@@ -3,54 +3,87 @@ from datetime import date
 
 class User(object):
 	"""
+	Returns User object
 
+	Attribute:
+		* email of the user
+		* fist_name of user
+		* last_name of user
+
+	Methods:
+		* full_name -> returns full name of user
+		* posts -> returns list of posts the user was author of
 	"""
 
-	all_posts = []
-
 	def __init__(self, email, first_name, last_name):
-		self.email = str(email)
-		self.first_name = str(first_name)
-		self.last_name = str(last_name)
-		self.posts = self.posts()
+		self.email = email
+		self.first_name = first_name
+		self.last_name = last_name
 
 	def full_name(self):
 		return self.first_name + ' ' + self.last_name
 
 	def posts(self):
+		all_posts = []
 		for single_post in Post.list_of_posts:
 			if self == single_post.author:
-				self.all_posts.append(single_post)
-		return self.all_posts
+				all_posts.append(single_post)
+		return all_posts
+
+	def __repr__(self):
+		return self.full_name()
 
 class Post(object):
 	"""
+	Returns Post object
 
+	Static attribute:
+		* list of all posts
+
+	Attribute:
+		* title of post
+		* body of post -> content of post
+		* publish_date 
+		* created_at (datetime)
+		* author -> user who wrote post
+
+	Methods:
+		* is_published -> returns bool 
+		* save -> to save all posts in static attribute
 	"""
 	list_of_posts = []
 
-	def __init__(self, title, body, User, year=0, month=0, day=0):
-		self.title = str(title)
-		self.body = str(body)
+	def __init__(self, title, body, user, year=0, month=0, day=0):
+		self.title = title
+		self.body = body
 		try:
 			self.publish_date = date(year, month, day)
 		except ValueError:
 			self.publish_date = ''
 		self.created_at = datetime.now()
-		self.author = User
-		Post.list_of_posts.append(self)
+		self.author = user
+		self.save()
 
 	def is_published(self):
-		return self.publish_date != ''
+		return self.publish_date != '' and self.publish_date <= date.today()
+
+	def save(self):
+		Post.list_of_posts.append(self) # TODO: duplikate vermeiden
 
 
 class Comment(object):
 	"""
+	Returns Comment object
 
+	Attribute:
+		* email address of user
+		* body -> content of comment
+		* created_at
+		* post which was commented
 	"""
 
-	def __init__(self, body, User, Post):
-		self.author_email = User.email
-		self.body = str(body)
+	def __init__(self, body, user, post):
+		self.author_email = user.email
+		self.body = body
 		self.created_at = datetime.now()
-		self.post = Post
+		self.post = post
