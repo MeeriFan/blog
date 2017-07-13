2bc8643bafbd314bd276efc7a28a380dde2bbc50
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-13 14:39:15 +0200

    simplified folder structure

--- run.py
+++ run.py
@@ -1,6 +1,15 @@
-from app import app
-from bottle import run
-from bottle import debug
+
+from bottle import template, Bottle, debug, run
+
+app = Bottle()
+
+@app.route('/')
+@app.route('/index')
+@app.route('/index/<name>')
+def greet(name='Stranger'):
+	#return template('Hello {{t_name}}, how are you?', t_name=name)
+	info = {'t_name' : name, 'title' : 'Welcome'}
+	return template('welcome_template.tpl', info)
 
 debug(True)
 run(app, host='localhost', port=8080, reloader=True)
