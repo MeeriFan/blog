db583f0b6c30f234e16dff798649065f2c2cca4b
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-14 11:54:55 +0200

    added login

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 
-from bottle import template, Bottle, debug, run
+from bottle import template, Bottle, debug, run, request,response
 
 app = Bottle()
 
@@ -27,6 +27,52 @@ def greet(name='Stranger'):
 	}
 	return template('index.tpl', info)
 
+
+@app.route('/profile')
+def profile():
+	# das hier geht nur, wenn man eingeloggt ist!
+	username = request.get_cookie('logged_in_as', False)
+	if not username:
+		return 'You must be logged in!'
+	else:
+		return 'Welcome back, ' + username
+
+@app.route('/login')
+def login_form():
+	info = {
+		'title' : 'Login',
+		'message' : 'Please log in.'
+	}
+	return template('login.tpl', info)
+
+@app.post('/login')
+def do_login():
+	admin = {
+		'admin_mail' : 'admin@google.net',
+		'password' : 'adminpassword'
+	}
+
+	email = request.forms.get('email')
+	pw = request.forms.get('pw')
+	if email and pw:
+		response.set_cookie('user_email', email)
+		response.set_cookie('user_pw', pw)
+
+	user = request.get_cookie('user_email', False)
+	user_pw = request.get_cookie('user_pw', False)
+
+	if user == admin['admin_mail'] and user_pw == admin['password']:
+		message = 'Welcome back!'
+		return template('registration.tpl', message=message)
+	else:
+		info = {
+			'title' : 'Login',
+			'message' : 'Login failed. Please try again.'
+		}
+		return template('login.tpl', info)
+
+	# hint: perform validation if login is correct
+	# hint: response.set_cookie
 
 if __name__ == '__main__':
 	debug(True)
