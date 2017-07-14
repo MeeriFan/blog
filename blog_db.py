90f80bc28893051f1cfc01e2381ba97735825e30
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-14 15:45:53 +0200

    user table created

--- blog_db.py
+++ blog_db.py
@@ -1,2 +1,15 @@
-import peewee
+from peewee import SqliteDatabase, Model
+from peewee import CharField
 
+db = SqliteDatabase('blog.db')
+
+class BaseModel(Model):
+	class Meta:
+		database = db
+
+class Users(BaseModel):
+	username = CharField()
+	email = CharField()
+	first_name = CharField()
+	last_name = CharField()
+	password = CharField()
