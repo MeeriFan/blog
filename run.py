0ac198e60a5ff9f10904ebfba124132d65c9b1f7
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-17 13:04:21 +0200

    handling registration of new user, deleting profiles and logout

--- run.py
+++ run.py
@@ -7,37 +7,21 @@ app = Bottle()
 
 def get_user_by_mail(u_email, u_pw):
 	try:
-		user = User.get(User.email == u_email, User.password == u_pw)
+		return User.get(User.email == u_email, User.password == u_pw)
 	except:
-		user = None
-	return user
+		return None
 
 def get_user(user_id):
 	return User.get(User.id == user_id)
 
 @app.route('/')
 @app.route('/index')
-@app.route('/index/<name>')
-def greet(name='Stranger'):
-	info = {
-		't_name' : name, 
-		'title' : 'Welcome',
-		'posts' : [
-			{
-				'author' : 'John',
-				'body' : 'Beautiful day in Portland!'
-			},
-			{
-				'author' : 'Susan',
-				'body' : 'The Avengers movie was so cool!' 
-			},
-			{
-				'author' : 'Tobias',
-				'body' : 'My new Trainee is the best!'
-			}
-		]
-	}
-	return template('index.tpl', info)
+def index():
+	session_key = request.get_cookie('session_id', secret=SECRET)
+	if session_key in session_dict:
+		return template('index.tpl', logged_in='yes')
+	else:
+		return template('index.tpl', logged_in='no')
 
 session_dict = {}
 
@@ -48,16 +32,27 @@ def profile():
 	# das hier geht nur, wenn man eingeloggt ist!
 	session_key = request.get_cookie('session_id', secret=SECRET)
 	if not session_key in session_dict:
-		return 'You must be logged in!'
+		info = {
+			'message' : 'You must be logged in.',
+			'logged_in' : 'no',
+			'href' : 'index'
+		}
+		return template('error.tpl', info)
 	else:
 		user = get_user(session_dict[session_key])
-		return 'Welcome back, ' + user.first_name + ' ' + user.last_name + '!'
+		message = 'Welcome back, ' + user.first_name +' '+ user.last_name + '!'
+		info = {
+			'message' : message,
+			'logged_in' : 'yes'
+		}
+		return template('profile.tpl', info)
 
 @app.route('/login')
 def login_form():
 	info = {
 		'title' : 'Login',
-		'message' : 'Please log in.'
+		'message' : 'Please log in.',
+		'logged_in' : 'no'
 	}
 	return template('login.tpl', info)
 
@@ -65,7 +60,8 @@ def login_form():
 def login_form_failed():
 	info = {
 		'title' : 'Login',
-		'message' : 'Login failed. Please try again.'
+		'message' : 'Login failed. Please try again.',
+		'logged_in' : 'no'
 	}
 	return template('login.tpl', info)
 
@@ -77,12 +73,90 @@ def do_login():
 	user = get_user_by_mail(email, pw)
 	if user:
 		key = uuid4().hex
-		response.set_cookie('session_id', key, secret=SECRET ,max_age=40)
+		response.set_cookie('session_id', key, secret=SECRET)
 		session_dict[key] = user.id
 		message = 'Welcome back, ' + user.username + '!'
-		return template('registration.tpl', message=message)
+		info = {
+			'message' : message,
+			'logged_in' : 'yes'
+		}
+		return template('profile.tpl', info)
 	else:
 		return redirect('/login/failed')
+
+@app.route('/logout')
+def logout():
+	return template('logout.tpl', logged_in='yes')
+
+@app.post('/logout')
+def do_logout():
+	session_key = request.get_cookie('session_id', secret=SECRET)
+	del session_dict[session_key]
+	response.delete_cookie('session_id', secret=SECRET)
+	return redirect('/index')
+
+@app.route('/registration')
+def registration():
+	info = {
+		'title' : 'Registration',
+		'message' : 'Please fill out the form completely for registration.',
+		'logged_in' : 'no'
+	}
+	return template('registration.tpl', info)
+
+@app.post('/registration')
+def do_registration():
+	u_f_name = request.forms.get('first_name')
+	u_l_name = request.forms.get('last_name')
+	u_name = request.forms.get('nickname')
+	u_email = request.forms.get('email')
+	u_pw = request.forms.get('pw')
+	user_known = get_user_by_mail(u_email, u_pw)
+	if not user_known:
+		new_user = User(
+			username=u_name, 
+			email=u_email, 
+			first_name=u_f_name,
+			last_name=u_l_name,
+			password=u_pw
+		)
+		new_user.save()
+		key = uuid4().hex
+		response.set_cookie('session_id', key, secret=SECRET)
+		session_dict[key] = new_user.id
+		info = {
+			'f_name' : u_f_name,
+			'l_name' : u_l_name,
+			'logged_in' : 'yes'
+		}
+		return template('thank_you.tpl', info)
+	else:
+		info = {
+			'message' : 'You are already registered.',
+			'href' : 'registration',
+			'logged_in' : 'no'
+		}
+		return template('error.tpl', info)
+
+@app.route('/delete')
+def delete_user():
+	info = {
+		'title' : 'Good Bye?',
+		'message' : 'You really want to delete you profile?',
+		'logged_in' : 'yes'
+	}
+	return template('delete.tpl', info)
+
+@app.post('/delete')
+def do_delete():
+	session_key = request.get_cookie('session_id', secret=SECRET)
+	user_id = session_dict[session_key]
+	old_user = get_user(user_id)
+	old_user.delete_instance()
+	del session_dict[session_key]
+	response.delete_cookie('session_id', secret=SECRET)
+	return template('sorry.tpl', logged_in='no')
+
 
 if __name__ == '__main__':
 	debug(True)
