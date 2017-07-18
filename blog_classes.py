7a701e83e0e301d5997e78ee1a7ea1a7db567fd5
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-18 15:31:25 +0200

    class user gets function if instance is valid

--- blog_classes.py
+++ blog_classes.py
@@ -20,5 +20,22 @@ class User(BaseModel):
 	class Meta:
 		db_table = 'users'
 
-	def has_email(self):
-		return self.email != None
+	def is_valid(self, repeated_pw):
+		for attribute in self:
+			if attribute == '':
+				message = 'You have to fill out all fields.'
+				return False, message
+		if not len(self.password) >= 8:
+			message = 'Your password needs to be at least 8 characters long.'
+			return False, message
+		if not any(c.isalpha() for c in self.password):
+			message = 'Your password should at least contain one alphabetic character.'
+			return False, message
+		if not self.password == repeated_pw:
+			message = 'The password and repeated password have to be the same.'
+			return False, message
+		if not '@' in self.email and not '.' in self.email:
+			message = 'Your email address is not valid.'
+			return False, message
+		return True, None
+
