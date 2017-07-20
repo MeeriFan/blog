87b872fe6ac36b66ba607e18ad5fdd7c2da61aa1
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-20 12:59:09 +0200

    new GET for users and users/id to get a list of all users

--- run.py
+++ run.py
@@ -1,59 +1,34 @@
-import peewee
-import hashlib
-from bottle import template, Bottle, debug, run, request,response, redirect, static_file
+from bottle import template, Bottle, debug, run, request, response
+from bottle import redirect, static_file
 from uuid import uuid4
 from blog_classes import User
 
 app = Bottle()
 
-def get_user_by_mail(u_email, u_pw):
-	u_pw = do_hash(u_pw)
-	try:
-		return User.get(User.email == u_email, User.password == u_pw)
-	except:
-		return None
 
-def get_user(user_id):
-	return User.get(User.id == user_id)
-
-def register_validation(user_register):
-	for key in user_register:
-		if user_register[key] == '':
-			message = 'You have to fill out all fields.'
-			return False, message
-	if not len(user_register['u_pw']) >= 8:
-		message = 'The password needs to be at least 8 signs long.'
-		return False, message
-	if not any(c.isalpha() for c in user_register['u_pw']):
-		message = 'Your password should at least contain one alphabetic character.'
-		return False, message
-	if not user_register['u_pw'] == user_register['u_pw_r']:
-		message = 'The password and repeated password have to be the same.'
-		return False, message
-	if not '@' in user_register['u_email'] and not '.' in user_register['u_email']:
-		message = 'Your email address is not valid.'
-		return False, message
-	return True, ''
-
-def setting_cookie(user_id):
+def set_app_cookie(user_id):
 	key = uuid4().hex
 	response.set_cookie('session_id', key, secret=SECRET)
 	session_dict[key] = user_id
 
-def deleting_cookie(delete_user=False):
+
+def delete_app_cookie(deactivate_user=False):
 	session_key = get_key()
+	if deactivate_user:
+		User.deactivate_user(session_dict[session_key])
 	del session_dict[session_key]
 	response.delete_cookie('session_id', secret=SECRET)
-	if delete_user:
-		user_id = session_dict[session_key]
-		old_user = get_user(user_id)
-		old_user.delete_instance()
 
-def get_key():	
+
+def get_key():
 	return request.get_cookie('session_id', secret=SECRET)
 
-def do_hash(password):
-	return hashlib.sha256(password.encode()).hexdigest()
+
+def user_logged_in():
+	if get_key():
+		return 'yes'
+	return 'no'
+
 
 @app.route('/')
 @app.route('/index')
@@ -68,136 +43,206 @@ session_dict = {}
 
 SECRET = 'dogcatmouse'
 
+
 @app.route('/profile')
 def profile():
-	# das hier geht nur, wenn man eingeloggt ist!
 	session_key = get_key()
-	if not session_key in session_dict:
+	if session_key not in session_dict:
 		info = {
-			'message' : 'You must be logged in.',
-			'logged_in' : 'no',
-			'href' : 'index'
+			'message': 'You must be logged in.',
+			'logged_in': 'no',
+			'href': 'index'
 		}
 		return template('error.tpl', info)
 	else:
-		user = get_user(session_dict[session_key])
-		message = 'Welcome back, ' + user.first_name +' '+ user.last_name + '!'
+		user = User.get_user(session_dict[session_key])
+		message = 'Welcome back, ' + user.first_name + ' ' + user.last_name + '!'
 		info = {
-			'message' : message,
-			'logged_in' : 'yes'
+			'message': message,
+			'logged_in': 'yes'
 		}
 		return template('profile.tpl', info)
+
+
+@app.route('/users')
+def list_users():
+	list_users = User.get_all()
+	info = {
+		'title': 'List of all Users',
+		'logged_in': user_logged_in(),
+		'users': list_users
+	}
+	return template('users.tpl', info)
+
+
+@app.route('/users/<user_id:int>')
+def specific_user(user_id):
+	user = User.get_user(user_id)
+	info = {
+		'user': user,
+		'title': 'Profile of %s' % user.username,
+		'logged_in': user_logged_in()
+	}
+	return template('single_user.tpl', info)
+
 
 @app.route('/login')
 def login_form():
 	info = {
-		'title' : 'Login',
-		'message' : 'Please log in.',
-		'logged_in' : 'no'
+		'title': 'Login',
+		'message': 'Please log in.',
+		'logged_in': 'no'
 	}
 	return template('login.tpl', info)
+
 
 @app.route('/login/failed')
 def login_form_failed():
 	info = {
-		'title' : 'Login',
-		'message' : 'Login failed. Please try again.',
-		'logged_in' : 'no'
+		'title': 'Login',
+		'message': 'Login failed. Please try again.',
+		'logged_in': 'no'
 	}
 	return template('login.tpl', info)
 
+
+@app.route('/login/inactive')
+def account_inactive():
+	info = {
+		'title': 'Login',
+		'message': 'Your account is inactive. Please reactivate.',
+		'logged_in': 'no',
+		'link': 'Reactivate my account'
+	}
+	return template('login.tpl', info)
+
+
 @app.post('/login')
 @app.post('/login/failed')
+@app.post('/login/inactive')
 def do_login():
-	email = request.forms.get('email')
-	pw = request.forms.get('pw')
-	user = get_user_by_mail(email, pw)
-	if user:
-		setting_cookie(user.id)
+	user = User(
+		email=request.forms.get('email'),
+		password=request.forms.get('pw')
+	)
+	if user.verify_login():
+		user = User.by_email(user.email)
+		set_app_cookie(user.id)
 		message = 'Welcome back, ' + user.username + '!'
 		info = {
-			'message' : message,
-			'logged_in' : 'yes'
+			'message': message,
+			'logged_in': 'yes'
 		}
 		return template('profile.tpl', info)
 	else:
 		return redirect('/login/failed')
 
+
 @app.post('/logout')
 def do_logout():
-	deleting_cookie()
+	delete_app_cookie()
 	return redirect('/index')
 
+
 @app.route('/registration')
-def registration(message='Please fill out the form completely for registration.',user_info={}):
+def registration(message='Please fill out the form completely for \
+	registration.', user=User(), repeated_pw=''):
 	info = {
-		'title' : 'Registration',
-		'message' : message,
-		'logged_in' : 'no',
-		'u_f_name' : user_info.get('u_f_name', ''),
-		'u_l_name' : user_info.get('u_l_name', ''),
-		'u_name' : user_info.get('u_name', ''),
-		'u_email' : user_info.get('u_email', ''),
-		'u_pw' : user_info.get('u_pw', ''),
-		'u_pw_r' : user_info.get('u_pw_r', '')
+		'title': 'Registration',
+		'message': message,
+		'logged_in': 'no',
+		'u_f_name': user.first_name,
+		'u_l_name': user.last_name,
+		'u_name': user.username,
+		'u_email': user.email,
+		'u_pw': user.password,
+		'u_pw_r': repeated_pw
 	}
 	return template('registration.tpl', info)
 
+
 @app.post('/registration')
 def do_registration():
-	user_info =	{
-		'u_f_name' : request.forms.get('first_name'),
-		'u_l_name' : request.forms.get('last_name'),
-		'u_name' : request.forms.get('nickname'),
-		'u_email' : request.forms.get('email'),
-		'u_pw' : request.forms.get('pw'),
-		'u_pw_r' : request.forms.get('r_pw')
-	}
-	input_validated, message = register_validation(user_info)
-	if not input_validated:
-		return registration(message=message, user_info=user_info)
-	user_known = get_user_by_mail(user_info['u_email'], user_info['u_pw'])
-	if not user_known:
-		new_user = User(
-			username=user_info['u_name'], 
-			email=user_info['u_email'], 
-			first_name=user_info['u_f_name'],
-			last_name=user_info['u_l_name'],
-			password=do_hash(user_info['u_pw'])
-		)
-		new_user.save()
-		setting_cookie(new_user.id)
-		info = {
-			'f_name' : user_info['u_f_name'],
-			'l_name' : user_info['u_l_name'],
-			'logged_in' : 'yes'
-		}
-		return template('thank_you.tpl', info)
+	new_user = User(
+		username=request.forms.get('nickname'),
+		email=request.forms.get('email'),
+		first_name=request.forms.get('first_name'),
+		last_name=request.forms.get('last_name'),
+		password=request.forms.get('pw')
+	)
+	if not new_user.is_already_in_db():
+		repeated_passwpord = request.forms.get('r_pw')
+		valid, error = new_user.is_valid(repeated_passwpord)
+		if valid:
+			new_user.generate_salt()
+			new_user.password = new_user.hash_password()
+			new_user.save()
+			set_app_cookie(new_user.id)
+			info = {
+				'f_name': new_user.first_name,
+				'l_name': new_user.last_name,
+				'logged_in': 'yes',
+				'message': 'Thank you for registrating to this microblog!'
+			}
+			return template('thank_you.tpl', info)
+		else:
+			return registration(message=error, u=new_user, r_pw=repeated_passwpord)
 	else:
 		info = {
-			'message' : 'You are already registered.',
-			'href' : 'registration',
-			'logged_in' : 'no'
+			'message': 'You are already registered.',
+			'href': 'registration',
+			'logged_in': 'no'
 		}
 		return template('error.tpl', info)
 
-@app.route('/delete')
-def delete_user():
+
+@app.route('/deactivate')
+def deactivate_user():
 	info = {
-		'title' : 'Good Bye?',
-		'message' : 'You really want to delete you profile?',
-		'logged_in' : 'yes'
+		'title': 'Good Bye?',
+		'message': 'You really want to deactivate you profile? This means you have to \
+		register again to reactivate your profile. All your data will be kept.',
+		'logged_in': 'yes'
+	}
+	return template('deactivate.tpl', info)
+
+
+@app.post('/deactivate')
+def do_deactivate():
+	delete_app_cookie(deactivate_user=True)
+	info = {
+		'logged_in': 'no',
+		'specification': 'deactivated'
+	}
+	return template('sorry.tpl', info)
+
+
+@app.route('/delete')
+def delete_account():
+	info = {
+		'title': 'Good Bye?',
+		'message': 'You really want to delete your account? This means that your \
+		whole profile will be deleted and there is no way back. Are you sure? \
+		You should consider to deactivate your profile',
+		'logged_in': 'yes'
 	}
 	return template('delete.tpl', info)
 
+
 @app.post('/delete')
 def do_delete():
-	deleting_cookie(delete_user=True)
-	return template('sorry.tpl', logged_in='no')
+	User.delete_user(session_dict[get_key()])
+	delete_app_cookie()
+	info = {
+		'logged_in': 'no',
+		'specification': 'deleted'
+	}
+	return template('sorry.tpl', info)
+
 
 @app.route('/static/<path:path>')
 def static_files(path):
-    return static_file(path, 'static/')
+	return static_file(path, 'static/')
 
 if __name__ == '__main__':
 	debug(True)
