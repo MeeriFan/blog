225ed7bf0de24255eb679332aa303dc8cbee9591
Anne Lehmann <anne.lehmann@grandcentrix.net>
2017-07-21 12:42:44 +0200

    added new post class to create or delete posts table in db

--- create_blog_db.py
+++ create_blog_db.py
@@ -1,14 +1,15 @@
 import peewee
-from blog_classes import User
+from blog_classes import User, Post
 from blog_classes import db
 
+
 def create_tables():
-	db.create_tables(
-		[User],
-		safe=True)
+    db.create_tables(
+        [User, Post],
+        safe=True)
 
 if __name__ == '__main__':
-	print('Creating tables...')
-	db.connect()
-	create_tables()
-	db.close()
+    print('Creating tables...')
+    db.connect()
+    create_tables()
+    db.close()
--- delete_blog_db.py
+++ delete_blog_db.py
@@ -1,14 +1,15 @@
 import peewee
-from blog_classes import User
-from blog_classes import db 
+from blog_classes import User, Post
+from blog_classes import db
+
 
 def delete_tables():
-	db.drop_tables(
-		[User],
-		safe=True)
+    db.drop_tables(
+        [User, Post],
+        safe=True)
 
 if __name__ == '__main__':
-	print('Dropping tables...')
-	db.connect()
-	delete_tables()
-	db.close()
+    print('Dropping tables...')
+    db.connect()
+    delete_tables()
+    db.close()
